8672049a982ed964e0cf8e93a2b87ccbf1ad2f07
silverknight59222 <silverknight59222@gmail.com>
2023-11-06 12:14:20 -0500

    fix data get

--- pages/api/data/[id].ts
+++ pages/api/data/[id].ts
@@ -7,7 +7,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
   switch (method) {
     case 'GET':
       try {
-        const querys = `SELECT data.*,program.name as program_name, train_history.status as status  from data left join program on program.id = data.program_id LEFT JOIN train on data.train_id = train.id left join train_history on train_history.train_id = train.id WHERE data.program_id = ${query.id} AND data.is_deleted = FALSE group by data.id,program.name, train_history.status order by data.id`;
+        const querys = `SELECT data.*,program.name as program_name, train_history.status as status  from data left join program on program.id = data.program_id left join train_history on train_history.data_id = data.id WHERE data.program_id = ${query.id} AND data.is_deleted = FALSE and train_history.train_id = data.train_id group by data.id,program.name, train_history.status order by data.id `;
         const response = await db.query(querys);
         return res.json(response);
       } catch (error: any) {
