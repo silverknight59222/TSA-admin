c81b3b5c2828630590415043e2e3d0c71802a987
silverknight59222 <silverknight59222@gmail.com>
2023-11-09 14:20:47 -0500

    fix query

--- pages/api/history/train/index.ts
+++ pages/api/history/train/index.ts
@@ -3,7 +3,7 @@ import db from '@/utils/database';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const { method, body, query } = req;
+  const { method, body } = req;
   switch (method) {
     case 'PUT':
       try {
