de63250ab249b99c8d0c172e172162d1ae723144
silverknight59222 <silverknight59222@gmail.com>
2023-11-10 13:03:06 -0500

    fix

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -4,7 +4,9 @@ const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
 console.log(email, pass);
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
   auth: {
     user: email,
     pass
