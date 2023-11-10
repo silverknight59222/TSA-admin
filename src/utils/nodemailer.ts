dfcc3724814f74349a22ffbe0d2945a01d5db54f
silverknight59222 <silverknight59222@gmail.com>
2023-11-10 12:42:43 -0500

    fix

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -2,10 +2,11 @@ import nodemailer from 'nodemailer';
 
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
+console.log(email, pass);
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: email,
-    pass,
-  },
+    pass
+  }
 });
